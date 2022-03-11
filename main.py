a7184dc03ba334acebbd2364325cefe528215212
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2022-03-12 02:07:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -103,8 +103,8 @@ def progress_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}] \n📊 <b>Progress:</b> {2}%\n".format(
-            ''.join([⬛ for i in range(math.floor(percentage / 10))]),
-            ''.join([⬜ for i in range(10 - math.floor(percentage / 10))]),
+            ''.join(["▓" for i in range(math.floor(percentage / 10))]),
+            ''.join(["░" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
         tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
